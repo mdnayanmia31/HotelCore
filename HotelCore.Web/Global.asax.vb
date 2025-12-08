3206ae4edb45c41ffceffd914d394ceb70186b61
Md Nayan Mia <123564739+mdnayanmia31@users.noreply.github.com>
2025-12-08 19:52:46 +0600

    Refactor Global.asax Error

--- HotelCore.Web/Global.asax.vb
+++ HotelCore.Web/Global.asax.vb
@@ -1,26 +1,24 @@
-Imports System.Web.Optimization
-Imports System.Web.Routing
+Imports System.Web. Optimization
+Imports System.Web. Routing
 
-Namespace HotelCore.Web
-    Public Class Global_asax
-        Inherits HttpApplication
+Public Class Global_asax
+    Inherits HttpApplication
 
-        Sub Application_Start(sender As Object, e As EventArgs)
-            RouteConfig.RegisterRoutes(RouteTable.Routes)
-            BundleConfig.RegisterBundles(BundleTable.Bundles)
-        End Sub
+    Sub Application_Start(sender As Object, e As EventArgs)
+        RouteConfig.RegisterRoutes(RouteTable.Routes)
+        BundleConfig.RegisterBundles(BundleTable.Bundles)
+    End Sub
 
-        Sub Application_Error(sender As Object, e As EventArgs)
-            Dim ex As Exception = Server.GetLastError()
-            If ex IsNot Nothing Then
-                Dim httpEx As HttpException = TryCast(ex, HttpException)
-                Dim statusCode As Integer = 500
-                If httpEx IsNot Nothing Then
-                    statusCode = httpEx.GetHttpCode()
-                End If
-                Server.ClearError()
-                Response.Redirect("~/Error.aspx?code=" & statusCode.ToString(), False)
+    Sub Application_Error(sender As Object, e As EventArgs)
+        Dim ex As Exception = Server.GetLastError()
+        If ex IsNot Nothing Then
+            Dim httpEx As HttpException = TryCast(ex, HttpException)
+            Dim statusCode As Integer = 500
+            If httpEx IsNot Nothing Then
+                statusCode = httpEx.GetHttpCode()
             End If
-        End Sub
-    End Class
-End Namespace
+            Server.ClearError()
+            Response.Redirect("~/Error.aspx?code=" & statusCode.ToString(), False)
+        End If
+    End Sub
+End Class
